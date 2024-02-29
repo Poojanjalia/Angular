8a90ce70ca9867b0c6d8e5e0f0f89d7730af4402
unknown <jaliapoojan@gmail.com>
2024-02-29 12:37:07 +0530

    first commit

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -8,25 +8,35 @@ import { SelectableMode, SelectableSettings } from '@progress/kendo-angular-grid
 })
 export class TableComponent {
   public selectedItems: any[] = [];
-  public selectedColumn : string []= ['ID', 'Name', 'Email', 'Age'];
+  public selectedColumn : string []= ['ID', 'Name', 'Email', 'Number'];
   public gridData: any[] = [
     {
       ID: 1,
       Name: "P",
       Email: "p@gmail.com",
       Age: 5,
+      Number: 9172481871
     },
     {
       ID: 2,
       Name: "x",
       Email: "x@gmail.com",
-      Age: 9
+      Age: 9,
+      Number: 9978886614
     },
     {
       ID: 3,
       Name: "Xs",
       Email: "xs@gmail.com",
-      Age: 6
+      Age: 6,
+      Number: 9428945624
+    },
+    {
+      ID: 4,
+      Name: "Xr",
+      Email: "xr@gmail.com",
+      Age: 8,
+      Number: 9428945624
     }
   ]
   public mode: SelectableMode = "multiple";
@@ -44,11 +54,16 @@ export class TableComponent {
     };
   }
   
-  public selectedRowData: any = [];
-  public onSelectionChange(selection: any):void {
-    if (selection.selectedRows.length > 0) {
-      const selectedItem = selection.selectedRows[0].dataItem;
-      this.selectedRowData = selectedItem;
+  public selectedRowData: any[] = [];
+
+  public onSelectionChange(event: any): void {
+    const selectedItems = event.selectedRows.map((row: any) => row.dataItem);
+  
+    if (event.deselectedRows.length > 0) {
+      const deselectedItems = event.deselectedRows.map((row: any) => row.dataItem);
+      this.selectedRowData = this.selectedRowData.filter(item => !deselectedItems.includes(item));
     }
+  
+    this.selectedRowData = [...this.selectedRowData, ...selectedItems]; //spread operator
   }
 }
